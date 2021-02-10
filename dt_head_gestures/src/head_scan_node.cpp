45023049355eb2eca7f1c738575e2bf8d506a7bd
guilhembn <gbuisan@laas.fr>
2021-02-10 15:19:33 +0100

    [HeadScan] Changes service to action

--- dt_head_gestures/src/head_scan_node.cpp
+++ dt_head_gestures/src/head_scan_node.cpp
@@ -2,16 +2,18 @@
 #include <pr2_head_manager_msgs/StateMachineRegister.h>
 #include <resource_management_msgs/StateMachinesStatus.h>
 #include <resource_management_msgs/StateMachinesCancel.h>
-#include <dt_head_gestures/HeadScan.h>
+#include <actionlib/server/simple_action_server.h>
+#include <dt_head_gestures/HeadScanAction.h>
 
 
 class HeadScan{
 public:
-    HeadScan(const ros::NodeHandlePtr nh): nh_(nh), isScanOver_(false), currentId_(-1){
+    HeadScan(const ros::NodeHandlePtr nh): nh_(nh), isScanOver_(false), currentId_(-1), 
+        scan_server_(*nh_, "head_scan", boost::bind(&HeadScan::onScanRequest, this, _1), false), start_time_(0){
         head_sm_client_ = nh->serviceClient<pr2_head_manager_msgs::StateMachineRegister>("/pr2_head_manager/state_machines_register");
         head_sm_cancel_client_ = nh->serviceClient<resource_management_msgs::StateMachinesCancel>("/pr2_head_manager/state_machine_cancel");
-	fsm_status_sub_ = nh->subscribe("/pr2_head_manager/state_machine_status", 10, &HeadScan::onFSMStatus, this);
-        scan_server_  = nh_->advertiseService("head_scan", &HeadScan::onScanRequest, this);
+	    fsm_status_sub_ = nh->subscribe("/pr2_head_manager/state_machine_status", 10, &HeadScan::onFSMStatus, this);
+        scan_server_.start();
     };
 
     ~HeadScan(){
@@ -26,18 +28,20 @@ public:
         return head_sm_cancel_client_.call(srv_obj);
     }
 
-    bool onScanRequest(dt_head_gestures::HeadScanRequest& req, dt_head_gestures::HeadScanResponse& res){
+    void onScanRequest(const dt_head_gestures::HeadScanGoalConstPtr &goal){
+        dt_head_gestures::HeadScanResult res;
+        dt_head_gestures::HeadScanFeedback feedback;
         pr2_head_manager_msgs::StateMachineRegister srv_obj;
         pr2_head_manager_msgs::StateMachineRegisterRequest fsm;
         fsm.header.begin_dead_line = ros::Time::now() + ros::Duration(10.0);
         fsm.header.timeout = ros::Duration(120.0);
         fsm.header.priority.value = resource_management_msgs::MessagePriority::STANDARD;
-        double y_min = req.central_point.point.y - req.width / 2;
-        double y_max = req.central_point.point.y + req.width / 2;
-        double z_min = req.central_point.point.z - req.height / 2;
-        double z_max = req.central_point.point.z + req.height / 2;
-        std::string frame_id = req.central_point.header.frame_id;
-        ros::Time stamp = req.central_point.header.stamp;
+        double y_min = goal->central_point.point.y - goal->width / 2;
+        double y_max = goal->central_point.point.y + goal->width / 2;
+        double z_min = goal->central_point.point.z - goal->height / 2;
+        double z_max = goal->central_point.point.z + goal->height / 2;
+        std::string frame_id = goal->central_point.header.frame_id;
+        ros::Time stamp = goal->central_point.header.stamp;
 
         int y_way = 1;
         int yi = 0;
@@ -46,20 +50,20 @@ public:
         sprintf(buf, state_name_base, yi, zi);
         fsm.header.initial_state = buf;
         pr2_head_manager_msgs::StateMachineStatePoint* previousState = nullptr;
-        while (z_min + std::max(zi - 1, 0) * req.step_length < z_max){
-            while(y_min + std::max(yi - 1, 0) * req.step_length < y_max && y_min + yi * req.step_length - y_min >= -0.000001){
+        while (z_min + std::max(zi - 1, 0) * goal->step_length < z_max){
+            while(y_min + std::max(yi - 1, 0) * goal->step_length < y_max && y_min + yi * goal->step_length - y_min >= -0.000001){
                 pr2_head_manager_msgs::StateMachineStatePoint state;
                 sprintf(buf, state_name_base, yi, zi);
                 state.header.id = buf;
                 state.data.header.frame_id = frame_id;
                 state.data.header.stamp = stamp;
-                state.data.point.x = req.central_point.point.x;
-                state.data.point.y = y_min + yi * req.step_length;
-                state.data.point.z = z_min + zi * req.step_length;
+                state.data.point.x = goal->central_point.point.x;
+                state.data.point.y = y_min + yi * goal->step_length;
+                state.data.point.z = z_min + zi * goal->step_length;
                 if (previousState != nullptr){
                     resource_management_msgs::StateMachineTransition t;
                     t.next_state = buf;
-                    t.end_condition.duration = req.duration_per_point.data;
+                    t.end_condition.duration = goal->duration_per_point.data;
                     t.end_condition.timeout = ros::Duration(-1);
                     previousState->header.transitions.push_back(t);
                 }
@@ -74,7 +78,7 @@ public:
         if (previousState != nullptr){
             resource_management_msgs::StateMachineTransition t;
             t.next_state = "end";
-            t.end_condition.duration = req.duration_per_point.data;
+            t.end_condition.duration = goal->duration_per_point.data;
             t.end_condition.timeout = ros::Duration(-1);
             previousState->header.transitions.push_back(t);
         }else{
@@ -83,28 +87,43 @@ public:
         srv_obj.request = fsm;
         bool registerSuccess = false;
         currentId_ = -1;
+        start_time_ = ros::Time(0);
         registerSuccess = head_sm_client_.call(srv_obj);
         if (!registerSuccess){
-            res.success = false;
-            return false;
+            //res.success = false;
         }
         isScanOver_ = false;
         currentId_ = srv_obj.response.id;
         ros::Duration sleepTime = ros::Duration(0, 1000000);
         while ((!isScanOver_) && ros::ok()){
+            if (scan_server_.isPreemptRequested()){
+                cancel();
+                res.success = false;
+                res.action_end = ros::Time::now();
+                scan_server_.setPreempted(res);
+                return;
+            }
+            if (start_time_ != ros::Time(0)){
+                feedback.action_start = start_time_;
+                scan_server_.publishFeedback(feedback);
+            }
             sleepTime.sleep();
         }
         res.success = true;
-        return res.success;
+        res.action_end = ros::Time::now();
     }
 
     void onFSMStatus(const resource_management_msgs::StateMachinesStatus& msg){
         if (currentId_ == -1 || msg.id != currentId_){
 	        return;
         }
+        if (start_time_ == ros::Time(0)){
+            start_time_ = ros::Time::now();
+        }
         if (msg.state_name == ""){
             isScanOver_ = true;
             currentId_ = -1;
+            start_time_ = ros::Time(0);
         }
     }
 
@@ -112,11 +131,12 @@ private:
     ros::NodeHandlePtr nh_;
     ros::ServiceClient head_sm_client_;
     ros::ServiceClient head_sm_cancel_client_;
-    ros::ServiceServer scan_server_;
+    actionlib::SimpleActionServer<dt_head_gestures::HeadScanAction> scan_server_;
     ros::Subscriber fsm_status_sub_;
     int currentId_;
 
     bool isScanOver_;
+    ros::Time start_time_;
 
     static constexpr const char* state_name_base = "scan_state_y%d_z%d";
 
